f07ec01a914b4033e3bacfcfe10a3fb788ed1c0f
Gbneto <71955743+gbneto65@users.noreply.github.com>
2020-10-19 11:26:40 +0200

    first upload

---
+++ setup_charts_parameters.py
@@ -0,0 +1,24 @@
+######################################################################################33
+# Setup of charts
+cost_color_1 =['r','b','r'] # line color of cost charts 1 
+earn_color_1 =['r','b','r'] # line color of earnings charts 1
+delta_color_1 =['r','b','r'] # line color of delta charts 1
+
+
+fontsize_label_1 =10
+fontsize_title_1 =11
+fontize_x_axis_1 =10
+fontsize_y_axis_1 =10
+fontsize_legend_1 =6
+fontsize_annot_1 =8
+fontsize_copyright =8
+
+fontstyle_label_1 ='normal'  # normal italic oblique
+fontstyle_title_1 ='normal'
+fontstyle_x_axis_1 ='normal'
+fontstyle_y_axis_1 ='normal'
+fontstyle_legend_1 ='normal'
+fontstyle_annot_1 ='normal'
+fontstyle_copyright ='normal'
+
+dpi_charts = 200 # resolution of the graph files
